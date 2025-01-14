2406becc9eb12660baf329ceda31ce8500ca0326
Wilson Padilla <wdavidw26@gmail.com>
2025-01-14 15:03:38 -0500

    :tada: init project

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:cinemapedia/config/theme/app_theme.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      routerConfig: appRouter,
+    );
+  }
+}
---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenMovies extends StatelessWidget {
+  static const name = 'home-screen';
+
+  const HomeScreenMovies({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Placeholder(),
+    );
+  }
+}
